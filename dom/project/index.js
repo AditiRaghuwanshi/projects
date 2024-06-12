a8c83ac967d85cb6ca3cc6d03a31ef8bebf1c1bf
Aditi Raghuwanshi <aditi.raghuwanshi430@nmims.edu.in>
2024-06-12 12:50:32 +0530

    1st project

---
+++ dom/project/index.js
@@ -0,0 +1,15 @@
+var statusText = document.querySelector("h5");
+var addFriend = document.querySelector("#add");
+
+flag = 0
+addFriend.addEventListener("click", function()
+  {
+    
+    statusText.innerHTML = "Friends";
+    statusText.style.color = "green"
+  
+  
+
+});
+
+
